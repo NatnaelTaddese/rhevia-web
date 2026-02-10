816fe50625561661c714c0724b67344d7d54dd98
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-11 01:51:33 +0800

    remote elysia types exported by eden

---
+++ types/server.d.ts
@@ -0,0 +1,97 @@
+import { Elysia } from "elysia";
+declare const app: Elysia<"", {
+    decorator: {};
+    store: {};
+    derive: {};
+    resolve: {};
+}, {
+    typebox: {};
+    error: {};
+} & {
+    typebox: {};
+    error: {};
+} & {
+    typebox: {};
+    error: {};
+} & {
+    typebox: import("@sinclair/typebox").TModule<{}>;
+    error: {};
+}, {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+} & {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+} & {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+    response: {};
+} & {
+    schema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+}, {
+    get: {
+        body: unknown;
+        params: {};
+        query: unknown;
+        headers: unknown;
+        response: {
+            200: string;
+        };
+    };
+} & {
+    [x: string]: {
+        get: {
+            body: unknown;
+            params: {};
+            query: unknown;
+            headers: unknown;
+            response: {
+                200: string;
+            };
+        };
+    };
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+} & {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+} & {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+    response: {};
+} & {
+    derive: {};
+    resolve: {};
+    schema: {};
+}>;
+export default app;
+export type App = typeof app;
