7d6f6666c339f1b7ab9a83726a6a3eb9d4c3f87f
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-11 12:17:06 +0800

    linting

--- types/server.d.ts
+++ types/server.d.ts
@@ -1,98 +1,106 @@
 import { Elysia } from "elysia";
-declare const app: Elysia<"", {
+declare const app: Elysia<
+  "",
+  {
     decorator: {};
     store: {};
     derive: {};
     resolve: {};
-}, {
+  },
+  {
     typebox: {};
     error: {};
-} & {
+  } & {
     typebox: {};
     error: {};
-} & {
+  } & {
     typebox: {};
     error: {};
-} & {
+  } & {
     typebox: import("@sinclair/typebox").TModule<{}>;
     error: {};
-}, {
+  },
+  {
     schema: {};
     standaloneSchema: {};
     macro: {};
     macroFn: {};
     parser: {};
     response: {};
-} & {
+  } & {
     schema: {};
     standaloneSchema: {};
     macro: {};
     macroFn: {};
     parser: {};
     response: {};
-} & {
+  } & {
     schema: {};
     standaloneSchema: {};
     macro: {};
     macroFn: {};
     parser: {};
     response: {};
-} & {
+  } & {
     schema: {};
     macro: {};
     macroFn: {};
     parser: {};
-}, {
+  },
+  {
     get: {
+      body: unknown;
+      params: {};
+      query: unknown;
+      headers: unknown;
+      response: {
+        200: string;
+      };
+    };
+  } & {
+    [x: string]: {
+      get: {
         body: unknown;
         params: {};
         query: unknown;
         headers: unknown;
         response: {
-            200: string;
+          200: string;
         };
+      };
     };
-} & {
-    [x: string]: {
-        get: {
-            body: unknown;
-            params: {};
-            query: unknown;
-            headers: unknown;
-            response: {
-                200: string;
-            };
-        };
-    };
-}, {
+  },
+  {
     derive: {};
     resolve: {};
     schema: {};
     standaloneSchema: {};
     response: {};
-}, {
+  },
+  {
     derive: {};
     resolve: {};
     schema: {};
     standaloneSchema: {};
     response: {};
-} & {
+  } & {
     derive: {};
     resolve: {};
     schema: {};
     standaloneSchema: {};
     response: {};
-} & {
+  } & {
     derive: {};
     resolve: {};
     schema: {};
     standaloneSchema: {};
     response: {};
-} & {
+  } & {
     derive: {};
     resolve: {};
     schema: {};
-}>;
+  }
+>;
 export default app;
 export type App = typeof app;
 
