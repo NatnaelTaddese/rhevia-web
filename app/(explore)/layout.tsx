e801e8679f5d89d4444fbee15d80bdd217922df5
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-18 06:32:24 +0800

    client side header

--- app/(explore)/layout.tsx
+++ app/(explore)/layout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "@/components/header";
+
 export default function ExploreLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+    </div>
+  );
 }
