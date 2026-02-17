a0eb38a8b0394c46500a3edfff14c7a4d7164738
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-02-18 05:30:25 +0800

    use session

--- app/(explore)/layout.tsx
+++ app/(explore)/layout.tsx
@@ -1,17 +1,7 @@
-import { SessionProvider } from "@/components/session-provider";
-import { Header } from "@/components/explore/header";
-
 export default function ExploreLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SessionProvider>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1">{children}</main>
-      </div>
-    </SessionProvider>
-  );
+  return <div>{children}</div>;
 }
--- app/(explore)/page.tsx
+++ app/(explore)/page.tsx
@@ -1,6 +1,34 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+
 export default function Page() {
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch, //refetch the session
+  } = authClient.useSession();
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!session) {
+    return (
+      <div>
+        Please <Link href="/login">sign in</Link>
+      </div>
+    );
+  }
+
   return (
     <>
+      <h1>{session?.user?.name}</h1>
       <h1>Explore</h1>
     </>
   );
